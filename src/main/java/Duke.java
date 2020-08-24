d85f1ba6c0aa7d0aaea6c08cb5135d364502957d
WYing333 <e0634980@u.nus.edu>
2020-08-24 20:13:58 +0800

    add increment: Level 2-- Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,19 +17,35 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
 
+        String[] userInput = new String[100];
+        int inputCount=0;
+        int outputCount=1;
+
         String line;
         System.out.print("Type something: ");
         line = in.nextLine();
 
-        while (!line.equals("bye")) {
-            System.out.println(line);
+        while ( !line.equals("bye") ) {
+            if (!line.equals("list")) {
+                userInput[inputCount] = line;
+                inputCount++;
+                System.out.println("Added:" + line);
+            }
+
+            if (line.equals("list")) {
+                while (outputCount<=inputCount) {
+                    System.out.println( outputCount + "." + userInput[outputCount-1] );
+                    outputCount++;
+                }
+                outputCount=1;
+            }
+
             System.out.println("____________________________________________________________");
             System.out.print("Type something: ");
             line = in.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
-
         System.out.println("____________________________________________________________");
 
 
