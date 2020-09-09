e995a4e0c566e5fdc332f8912f43a50baf9bd500
WYing333 <e0634980@u.nus.edu>
2020-09-09 18:29:26 +0800

    A-Packages:  Organize into Packages

--- src/main/java/duke/exception/emptyException.java
+++ src/main/java/duke/exception/emptyException.java
@@ -1,3 +1,5 @@
+package duke;
+
 public class emptyException extends DukeException{
         public emptyException() {
             System.out.println("The description of an action(exclude list) cannot be empty.");
--- src/main/java/duke/exception/nonMatchException.java
+++ src/main/java/duke/exception/nonMatchException.java
@@ -1,3 +1,5 @@
+package duke;
+
 public class nonMatchException extends DukeException {
     public nonMatchException() {
         System.out.println("I'm sorry, but I don't know what that means :-(");
